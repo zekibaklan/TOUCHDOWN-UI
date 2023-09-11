4604614e1af2b969965373deaf25bd0bf1674799
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-11 11:27:50 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Zeki Baklan on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
